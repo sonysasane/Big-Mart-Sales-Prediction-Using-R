8875d62d2fda61f2f9e7979713ee0c4ee5ad17cc
sonysasane <45103490+sonysasane@users.noreply.github.com>
2019-07-14 16:28:56 -0500

    Update EDA_NUMERICAL variables_part1.R
    EAD ON CATEGORICAL VARIABLES

--- EDA_NUMERICAL variables_part1.R
+++ EDA_NUMERICAL variables_part1.R
@@ -35,3 +35,52 @@ library(cowplot)
 
 plot_grid(p,q,r, nrow = 1) # plots all plots in one row it is in cowplot package
 
+# EAD on categorical variables
+# Item_Fat_Content variable
+ggplot(data = combi, aes(x = Item_Fat_Content)) + geom_bar(fill = "red")
+# from chart we know that there are only two categories low fat and regular
+
+combi$Item_Fat_Content[combi$Item_Fat_Content == 'LF'] <- "Low Fat"
+combi$Item_Fat_Content[combi$Item_Fat_Content == 'low fat'] <- "Low Fat"
+combi$Item_Fat_Content[combi$Item_Fat_Content == 'reg'] <- "Regular"
+str(combi)
+ggplot(data = combi, aes(x = Item_Fat_Content)) + geom_bar(fill = "red")
+combi$Item_Fat_Content
+levels(combi$Item_Fat_Content)
+
+# for other cate. variables
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("cowplot")
+library(dplyr)
+str(combi)
+a <- ggplot(combi %>% group_by(Item_Type) %>% summarise(Count = n())) +
+  geom_bar(aes(Item_Type, Count), stat = "identity",fill = "coral1") +
+  geom_label(aes(Item_Type, Count, label = Count), vjust = 0.5) + xlab("")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+  ggtitle("Item_Type")
+  
+b <- ggplot(combi %>% group_by(Outlet_Identifier) %>% summarise(Count = n())) +
+  geom_bar(aes(Outlet_Identifier, Count), stat = "identity", fill = "Darkgreen") +
+  geom_label(aes(Outlet_Identifier, Count, label = Count), vjust = 0.5) +
+  theme(axis.text.x = element_text(angle = 45,hjust = 1)) +ggtitle("Outlet_Identifier")
+
+c <- ggplot(combi %>% group_by(Outlet_Size) %>% summarise(Count = n())) +
+  geom_bar(aes(x = Outlet_Size, y = Count), stat = 'identity', fill = "Red") +
+  geom_label(aes(x = Outlet_Size, y = Count, label = Count), vjust = 0.5)+
+  theme(axis.text.x = element_text(angle = 45,hjust = 1)) + ggtitle("Outlet_Size")
+
+second_row <- plot_grid(b,c, nrow = 1)
+plot_grid(a,second_row, ncol = 1)
+
+d <- ggplot(combi %>% group_by(Outlet_Establishment_Year) %>% summarise(Count = n()))+
+  geom_bar(aes(factor(Outlet_Establishment_Year), Count),stat = 'identity',fill = "Darkblue") +
+  geom_label(aes(factor(Outlet_Establishment_Year), Count, label = Count), vjust = 0.5) +
+  theme(axis.text.x = element_text(size = 8.5)) + xlab("Outlet_Establishment_Year")
+
+e <- ggplot(combi %>% group_by(Outlet_Type) %>% summarise(Count = n())) +
+  geom_bar(aes(Outlet_Type, Count), stat = 'identity',fill= "coral1") +
+  geom_label(aes(Outlet_Type, Count, label = Count), vjust = 0.5) +
+  theme(axis.text.x = element_text(size = 8.5))
+
+
+plot_grid(d,e, ncol = 2)
